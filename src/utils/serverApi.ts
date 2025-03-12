ae4ab655b63dda22c17e87977beba48aa773b62d
siheon <rlatlgjs357@gmail.com>
2025-03-12 16:33:43 +0900

    serverApi fetchItemList 작성

--- src/utils/serverApi.ts
+++ src/utils/serverApi.ts
@@ -1,5 +1,31 @@
+const API_VERSION = "15.5.1";
+
 export const fetchChampionList = () => {};
 
 export const fetchChampionDetail = () => {};
 
-export const fetchItemList = () => {};
+export const fetchItemList = async () => {
+  const res = await fetch(
+    `https://ddragon.leagueoflegends.com/cdn/${API_VERSION}/data/ko_KR/item.json`
+  );
+
+  if (!res.ok) {
+    throw new Error("아이템 데이터를 불러오는 데 실패했습니다.");
+  }
+
+  const data = await res.json();
+  return Object.entries(data.data).map(([id, item]: [string, any]) => ({
+    id,
+    name: item.name,
+    plaintext: item.plaintext,
+    image: {
+      full: item.image.full,
+      sprite: item.image.sprite,
+      group: item.image.group,
+      x: item.image.x,
+      y: item.image.y,
+      w: item.image.w,
+      h: item.image.h,
+    },
+  }));
+};
